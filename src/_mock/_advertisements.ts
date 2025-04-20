bbeab0e7a0b8f6896c8d0a79d1012f7ccb037f0b
alaaahmade <alaaahmade66@gmail.com>
2025-04-20 13:40:27 +0300

    mock data

---
+++ src/_mock/_advertisements.ts
@@ -0,0 +1,66 @@
+export const _advertisements = [
+  {
+    id: 20,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 1,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 2,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 3,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 4,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 5,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 6,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 7,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 8,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 9,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+  {
+    id: 10,
+    image: 'https://s3-alpha-sig.figma.com/img/388a/4284/302b32115afa54a1542db4d7a96cbdcf?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pqAZqvrD74E6O9gVmD7foyTNP6j5BAMLviY1y502oPz7gcOsLVEgI5suFWHM3SW7HC3qc8imqyU2HNQCAn~bdm~N80d5WmkZBB~u4jFYKLQbM~t~sUR4ru4~x1U9PggMc-6200Lx5R4qJoZAkTvTDSjkNMAzh8hIt~fHYG3RimHvDrdgBhALUe7vr6PP9AWWj1fX3Mfrrg0~lmCBVCel~adPUiMpxvrVEPZ2CS20ehqzsw27q7sAZePwRBLKvd1XTg02KOpBPIeVSJTdncMexR4b3EgtLgwZeZEylfLV0FVLa2GPgYMkeXU0Wg56X98I8ziIkfNveKvc44kgEky6iw__',
+    title: 'Get the best of this deal',
+    url: 'https://example.com/example'
+  },
+
+]
---
+++ src/_mock/_subscriptions.ts
@@ -0,0 +1,295 @@
+import { _userList } from './_user';
+
+export const _subscriptions = [
+  {
+    id:'6079',
+    customer: _userList[0],
+    status: 'active',
+    date: new Date(),
+    category: 20,
+    price: '72.80',
+    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+  },
+  {
+    id:'6080',
+    customer: _userList[1],
+    category: 20,
+    date: new Date(),
+    status: 'active',
+    price: '27.80',    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+  },
+  {
+    id:'6081',
+    customer: _userList[3],
+    date: new Date(),
+    category: 20,
+    status: 'cancelled',    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    price: '850.00',
+  },
+  {
+    id:'6082',
+    customer: _userList[4],
+    date: new Date(),
+    status: 'active',    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    category: 20,
+    price: '72.80',
+  },
+  {
+    id:'6083',
+    customer: _userList[5],
+    date: new Date(),
+    category: 20,    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    status: 'active',
+    price: '27.80',
+  },
+  {
+    id:'6084',
+    customer: _userList[6],
+    date: new Date(),
+    category: 20,    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    status: 'active',
+    price: '72.80',
+  },
+  {
+    id:'6085',
+    customer: _userList[7],
+    date: new Date(),
+    category: 20,
+    status: 'active',    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    price: '27.80',
+  },
+  {
+    id:'6086',
+    customer: _userList[8],
+    date: new Date(),    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    category: 20,
+    status: 'cancelled',
+    price: '850.00',
+  },
+  {
+    id:'6087',
+    customer: _userList[9],
+    date: new Date(),    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    category: 20,
+    status: 'active',
+    price: '72.80',
+  },
+  {
+    id:'6088',
+    customer: _userList[10],
+    date: new Date(),
+    category: 20,    history: [
+      {
+        id: 0,
+        date: new Date(),
+        status: 'Failed',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 1,
+        status: 'Paid',
+        price: '72.80'
+      },
+      {
+        date: new Date('2022-01-01'),
+        id: 2,
+        status: 'paid',
+        price: '72.80'
+      },
+      
+
+    ],
+    status: 'active',
+    price: '27.80',
+  },
+]
